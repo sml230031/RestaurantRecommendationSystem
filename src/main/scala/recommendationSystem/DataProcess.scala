b6e045a44f2ea8127318bb1e01c70e47f7dd8bb0
Yi Liu <liuyi95@hotmail.com>
2018-11-29 15:55:53 -0500

    prepare training data and testing data

--- src/main/scala/recommendationSystem/DataProcess.scala
+++ src/main/scala/recommendationSystem/DataProcess.scala
@@ -1,6 +1,8 @@
 package recommendationSystem
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.to_date
+import org.apache.spark.sql.functions.lit
 
 object DataProcess {
 
@@ -12,7 +14,7 @@ object DataProcess {
       .getOrCreate()
 
     val df = spark.read.json("../finalproject/yelp-dataset/yelp_academic_dataset_business.json")
-    df
+    df.select("business_id", "name", "state", "city", "address")
   }
 
   def getReviewDataFrame(): DataFrame = {
@@ -23,13 +25,14 @@ object DataProcess {
       .getOrCreate()
 
     val df = spark.read.json("../finalproject/yelp-dataset/yelp_academic_dataset_review.json")
-    df
+    df.select("business_id", "user_id", "stars", "date")
+      .filter(to_date(df("date"),"yyyy-MM-dd").gt(lit("2015-01-01")))
   }
 
   def getUserDataFrame(): DataFrame = {
     val spark = SparkSession
       .builder
-      .appName("reviewDataFrame")
+      .appName("userDataFrame")
       .master("local[2]")
       .getOrCreate()
 
--- src/main/scala/recommendationSystem/Main.scala
+++ src/main/scala/recommendationSystem/Main.scala
@@ -1,5 +1,24 @@
 package recommendationSystem
 
-object Main extends App{
+object Main extends App {
+
+
+  val businessDataFrame = DataProcess.getBusinessDataFrame()
+  val reviewDataFrame = DataProcess.getReviewDataFrame()
+//  val userDataFrame = DataProcess.getUserDataFrame()
+
+  val businessDF = businessDataFrame
+  val reviewDF = reviewDataFrame
+
+  businessDF.createOrReplaceTempView("business")
+  reviewDF.createOrReplaceTempView("review")
+
+  val splits = reviewDF.randomSplit(Array(0.8, 0.2))
+  val (trainingData, testData) = (splits(0), splits(1))
+
+  println("TrainingNum" + trainingData.count())
+  println("TestingNum" + testData.count())
+  println(reviewDF.count())
+
 
 }
